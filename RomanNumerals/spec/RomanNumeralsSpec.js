93549e8405a25ec8915ae07429d1503e0d927c7c
Elena <elena.beccaro@gmail.com>
2020-06-29 18:16:02 +0100

    refractor and test passing

--- RomanNumerals/spec/RomanNumeralsSpec.js
+++ RomanNumerals/spec/RomanNumeralsSpec.js
@@ -7,6 +7,8 @@ describe("RomanNumerals", function() {
   it("convert numerals to Roman numerals", function() {
     expect(romanNumerals.fromNumber(1)).toBe("I");
     expect(romanNumerals.fromNumber(2)).toBe("II");
+    expect(romanNumerals.fromNumber(3)).toBe("III");
+    expect(romanNumerals.fromNumber(4)).toBe("IV");
   });
 });
 
--- RomanNumerals/src/RomanNumerals.js
+++ RomanNumerals/src/RomanNumerals.js
@@ -1,11 +1,17 @@
 function RomanNumerals() {};
 
+var numerlas = [
+  { value : 4, numeral: "IV"},
+  { value : 1, numeral: "I"},
+]
+
 RomanNumerals.prototype.fromNumber = function(n) {
   var result = "";
-  while (n > 0) {
-    result +="I";
-    --n;
-  }
-return result
+  numerlas.forEach(function(item) {
+    for (; n >= item.value; n -= item.value ) {
+      result += item.numeral; 
+    }
+  });
+  return result
 }
 
